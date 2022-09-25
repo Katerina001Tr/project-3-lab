4df64cab7ef7bfe4471836c2a6e3f4fb749f7145
Katerina001Tr <93012937+Katerina001Tr@users.noreply.github.com>
2022-09-25 12:45:54 +0500

    Update Фт_210008_Терентьева_шифр_Цезаря.py
    Ввели переменную итогового результата

--- Фт_210008_Терентьева_шифр_Цезаря.py
+++ Фт_210008_Терентьева_шифр_Цезаря.py
@@ -1,4 +1,4 @@
 alfavitEU = 'ABCDEFGHIJKLMNOPQRSTUVWXYZABCDEFGHIJKLMNOPQRSTUVWXYZ' 
 b=int(input('Введите шаг сдвига от 0 до 25: '))
 shifrovka=int(input('Вам необходимо: \n 1-Зашифровать текст \n 2-Расшифровать текст '))
-
+shifr=''

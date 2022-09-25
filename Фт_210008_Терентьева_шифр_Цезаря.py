c84d99affbb571ef30f0ae5c0fbd9101c9fdc827
Katerina001Tr <93012937+Katerina001Tr@users.noreply.github.com>
2022-09-25 12:48:36 +0500

    Update Фт_210008_Терентьева_шифр_Цезаря.py
    Написали цикл for для зашифровки входящего сообщения. Использовали поиск буквы в алфавите.

--- Фт_210008_Терентьева_шифр_Цезаря.py
+++ Фт_210008_Терентьева_шифр_Цезаря.py
@@ -2,3 +2,11 @@ alfavitEU = 'ABCDEFGHIJKLMNOPQRSTUVWXYZABCDEFGHIJKLMNOPQRSTUVWXYZ'
 b=int(input('Введите шаг сдвига от 0 до 25: '))
 shifrovka=int(input('Вам необходимо: \n 1-Зашифровать текст \n 2-Расшифровать текст '))
 shifr=''
+if shifrovka==1:
+        for i in input('Введите сообщение на английском языке, которое надо зашифровать: ').upper():
+            pol=alfavitEU.find(i)
+            newpol=pol+b 
+            if i in alfavitEU:
+                shifr+=alfavitEU[newpol]
+            else:
+                shifr+=i

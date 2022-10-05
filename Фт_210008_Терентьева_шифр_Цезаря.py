3be47325ba3edf37916dc890fddccd691b4232e7
Katerina001Tr <93012937+Katerina001Tr@users.noreply.github.com>
2022-10-05 21:54:10 +0500

    Update Фт_210008_Терентьева_шифр_Цезаря.py
    Комментарии

--- Фт_210008_Терентьева_шифр_Цезаря.py
+++ Фт_210008_Терентьева_шифр_Цезаря.py
@@ -1,19 +1,19 @@
-alfavitEU = 'ABCDEFGHIJKLMNOPQRSTUVWXYZABCDEFGHIJKLMNOPQRSTUVWXYZ' 
-b=int(input('Введите шаг сдвига от 0 до 25: '))
-if (b>=0) and (b<=25):
-        shifrovka=int(input('Вам необходимо: \n 1-Зашифровать текст \n 2-Расшифровать текст '))
+alfavitEU = 'ABCDEFGHIJKLMNOPQRSTUVWXYZABCDEFGHIJKLMNOPQRSTUVWXYZ'     #Задаю английский алфавит
+b=int(input('Введите шаг сдвига от 0 до 25: '))                          #Ввод шага сдвига
+if (b>=0) and (b<=25):                                                     #Условие для шага сдвига, чтобы программа выполнялась успешно
+        shifrovka=int(input('Вам необходимо: \n 1-Зашифровать текст \n 2-Расшифровать текст '))              #Диалоговый режим с пользователем
         shifr=''
         if shifrovka==1:
-          for i in input('Введите сообщение на английском языке, которое надо зашифровать: ').upper():
-            pol=alfavitEU.find(i)
-            newpol=pol+b 
+          for i in input('Введите сообщение на английском языке, которое надо зашифровать: ').upper():    #Диалоговый режим с пользователем при условии, что мы
+            pol=alfavitEU.find(i)                                                                           #шифруем сообщение.
+            newpol=pol+b                                             #Происходит поиск букв в заданном алфавите и смещение буквы для шифрования с шагом b.
             if i in alfavitEU:
                 shifr+=alfavitEU[newpol]
             else:
                 shifr+=i
         elif shifrovka==2:
-          for i in input('Введите сообщение, которое надо расшифровать: ').upper():
-            pol=alfavitEU.find(i)
+          for i in input('Введите сообщение, которое надо расшифровать: ').upper():           #Диалоговый режим с пользователем при условии, что мы 
+            pol=alfavitEU.find(i)                                                          #расшифровываем сообщение. Ищем буквы в строке и сдвигаем буквы с шагом b.
             newpol=pol-b 
             if i in alfavitEU:
                 shifr+=alfavitEU[newpol]
@@ -21,5 +21,5 @@ if (b>=0) and (b<=25):
                 shifr+=i
         print(shifr) 
 else:
-        print('Ошибка ввода')
+        print('Ошибка ввода')                          #Если значение вышло за диапазон, выводим сообщение об ошибке
         

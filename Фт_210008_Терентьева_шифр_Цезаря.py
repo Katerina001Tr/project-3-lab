17fc284035ae7928f96fc96444eefc4220cbfe14
Katerina001Tr <93012937+Katerina001Tr@users.noreply.github.com>
2022-10-05 21:45:12 +0500

    Update Фт_210008_Терентьева_шифр_Цезаря.py
    Интерфейс

--- Фт_210008_Терентьева_шифр_Цезаря.py
+++ Фт_210008_Терентьева_шифр_Цезаря.py
@@ -1,21 +1,25 @@
 alfavitEU = 'ABCDEFGHIJKLMNOPQRSTUVWXYZABCDEFGHIJKLMNOPQRSTUVWXYZ' 
 b=int(input('Введите шаг сдвига от 0 до 25: '))
-shifrovka=int(input('Вам необходимо: \n 1-Зашифровать текст \n 2-Расшифровать текст '))
-shifr=''
-if shifrovka==1:
-        for i in input('Введите сообщение на английском языке, которое надо зашифровать: ').upper():
+if (b>=0) and (b<=25):
+        shifrovka=int(input('Вам необходимо: \n 1-Зашифровать текст \n 2-Расшифровать текст '))
+        shifr=''
+        if shifrovka==1:
+          for i in input('Введите сообщение на английском языке, которое надо зашифровать: ').upper():
             pol=alfavitEU.find(i)
             newpol=pol+b 
             if i in alfavitEU:
                 shifr+=alfavitEU[newpol]
             else:
                 shifr+=i
-elif shifrovka==2:
-        for i in input('Введите сообщение, которое надо расшифровать: ').upper():
+        elif shifrovka==2:
+          for i in input('Введите сообщение, которое надо расшифровать: ').upper():
             pol=alfavitEU.find(i)
             newpol=pol-b 
             if i in alfavitEU:
                 shifr+=alfavitEU[newpol]
             else:
                 shifr+=i
-print(shifr) 
+        print(shifr) 
+else:
+        print('Ошибка ввода')
+        
